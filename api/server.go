a0b23c93be1ad334abadf201fcf238747d6cf7f5
viastolfi <vincent.astolfi12@gmail.com>
2025-07-16 16:42:06 +0200

    feat: add database connection

--- api/server.go
+++ api/server.go
@@ -6,14 +6,22 @@ import (
   "net/http"
 
   "github.com/gin-gonic/gin"
+
+  "main/database"
 )
 
 type Server struct {
   router *gin.Engine
+  dbManager *database.Manager
   jwtSecret string
 }
 
 func NewServer() (*Server, error) {
+  dbManager, err := database.NewManager()
+  if err != nil {
+    return nil, err
+  }
+
   jwtSecret := os.Getenv("SECRET_TOKEN")
   if jwtSecret == "" {
     log.Fatal("SECRET_TOKEN env variable is required")
@@ -21,6 +29,7 @@ func NewServer() (*Server, error) {
 
   server := &Server {
     router: gin.Default(),
+    dbManager: dbManager,
     jwtSecret: jwtSecret,
   }
 
@@ -44,6 +53,5 @@ func (s *Server) Run(addr ...string) error {
 }
 
 func (s *Server) Close() error {
-  // Close the Database with this
-  return nil
+  return s.dbManager.Close()
 }
